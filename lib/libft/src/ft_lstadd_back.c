9416aacefa9c72fa6f12b951b632354cab5e2358
Grzegorz Harazka <grzegorz.harazka@gmail.com>
2024-10-24 20:10:15 +0200

    added linked lists support

--- lib/libft/src/ft_lstadd_back.c
+++ lib/libft/src/ft_lstadd_back.c
@@ -17,5 +17,8 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	if (!ft_lstlast(*lst))
 		*lst = new;
 	else
+          {
 		ft_lstlast(*lst)->next = new;
+        new->next = NULL;
+                }
 }
